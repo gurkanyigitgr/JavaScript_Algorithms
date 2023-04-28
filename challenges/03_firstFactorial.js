3a2b897ab52f0a48e5b71450edaf5585a32efb6a
Gürkan <gurkanyigitgr@gmail.com>
2023-04-28 14:07:14 +0300

    third challenge

---
+++ challenges/03_firstFactorial.js
@@ -0,0 +1,17 @@
+export const firstFactorial = (num) => {
+  let factorial = 1;
+
+  for (let i = 2; i <= num; i++) {
+    factorial *= i;
+  }
+  return factorial;
+};
+
+/* 
+    First Factorial
+
+Using the Javascript language, have the function firstFactorial(num) take the num parameter being passed and return 
+the factorial of it (ie. if num = 4 , return (4*3*2*1)). For the test cases, the range will be between 1 and 18.
+
+
+*/
--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 import { userValidation } from "./challenges/01_userValidation.js";
 import { longestWord } from "./challenges/02_longestWord.js";
+import { firstFactorial } from "./challenges/03_firstFactorial.js";
 console.log(userValidation("u_hello_world123")); // output : true
 
 console.log(longestWord("fun?!! time")); // output : "time"
+
+console.log(firstFactorial(5));
