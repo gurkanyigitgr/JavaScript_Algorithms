3196cf6f8b489973bcce3f8df6e249ac5b8c0a19
Gürkan <gurkanyigitgr@gmail.com>
2023-05-18 10:19:20 +0300

    import js file

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ import { longestWord } from "./challenges/02_longestWord.js";
 import { firstFactorial } from "./challenges/03_firstFactorial.js";
 import { firstReverse } from "./challenges/04_firstReverse.js";
 import { findIntersection } from "./challenges/05_findIntersection.js";
+import { questionMark } from "./challenges/06_questionMark.js";
 
 console.log(userValidation("u_hello_world123")); // output : true
 
@@ -13,3 +14,5 @@ console.log(firstFactorial(5));
 console.log(firstReverse("Spectacular"));
 
 console.log(findIntersection(["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"]));
+
+console.log(questionMark("acc?7??sss?3rr1??????5"));
